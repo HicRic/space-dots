d8235a8c3f32f2a6ab72950a1f979858be630b5a
Ricky Willis <ricky.willis@spaceapegames.com>
2020-07-04 15:42:47 +0100

    Set sprite textures up. Add sprite atlas. Set up input + onscreen stick/button input.

---
+++ space-dots/Assets/Scripts/Input/InputHandler.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputHandler : MonoBehaviour
+{
+    public TextMeshProUGUI tmp;
+
+    public void OnMove(InputValue input)
+    {
+        Vector2 move = input.Get<Vector2>();
+        tmp.SetText(move.ToString());
+        Debug.Log("move: " + move);
+    }
+
+    public void OnFire(InputValue input)
+    {
+        tmp.SetText("fire!");
+        Debug.Log("fire!");
+    }
+}
