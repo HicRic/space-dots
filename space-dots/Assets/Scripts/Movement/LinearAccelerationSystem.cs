41be3de3662de214b3442490335724ecd144479f
Ricky Willis <ricky.willis@spaceapegames.com>
2020-08-15 17:08:15 +0100

    linear acceleration/velocity applied

---
+++ space-dots/Assets/Scripts/Movement/LinearAccelerationSystem.cs
@@ -0,0 +1,13 @@
+﻿using Unity.Entities;
+
+public class LinearAccelerationSystem : SystemBase
+{
+    protected override void OnUpdate()
+    {
+        float time = Time.DeltaTime;
+        Entities.ForEach((ref LinearVelocity velocity, in LinearAcceleration acceleration) =>
+        {
+            velocity.Value += acceleration.Value * time;
+        }).Schedule();
+    }
+}
---
+++ space-dots/Assets/Scripts/Movement/LinearVelocitySystem.cs
@@ -0,0 +1,17 @@
+﻿using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+public class LinearVelocitySystem : SystemBase
+{
+    protected override void OnUpdate()
+    {
+        float delta = Time.DeltaTime;
+        Entities.ForEach((ref Translation translation, in LinearVelocity velocity) =>
+        {
+            float2 moveDelta = velocity.Value * delta;
+            translation.Value += new float3(moveDelta, 0);
+        }).Schedule();
+    }
+}
