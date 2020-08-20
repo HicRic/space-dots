6d9947f98c442ed282f539dd1a00f5fc16dcaade
Ricky Willis <ricky.willis@spaceapegames.com>
2020-08-20 17:18:17 +0100

    Entities are destroyed when reachign their damage limit. Add a hack/workaround for an issue where destroying the final entity that has a collider causes the collider to remain in the world forever, pointing at a null entity.

---
+++ space-dots/Assets/Scripts/Lifetime/DamageLimit.cs
@@ -0,0 +1,8 @@
+﻿using Unity.Entities;
+
+[GenerateAuthoringComponent]
+public struct DamageLimit : IComponentData
+{
+    public int Value;
+}
+
---
+++ space-dots/Assets/Scripts/Lifetime/DamageLimitDestroySystem.cs
@@ -0,0 +1,30 @@
+﻿using Unity.Entities;
+using Unity.Jobs;
+
+public class DamageLimitDestroySystem : SystemBase
+{
+    private EndSimulationEntityCommandBufferSystem ecbs;
+
+    protected override void OnCreate()
+    {
+        base.OnCreate();
+
+        ecbs = World.GetOrCreateSystem<EndSimulationEntityCommandBufferSystem>();
+    }
+
+    protected override void OnUpdate()
+    {
+        EntityCommandBuffer ecb = ecbs.CreateCommandBuffer();
+
+        Entities.ForEach((Entity entity, in DamageLimit damageLimit, in DamageTaken damageTaken) =>
+        {
+            bool destroy = damageTaken.Value >= damageLimit.Value;
+            if (destroy)
+            {
+                ecb.DestroyEntity(entity);
+            }
+        }).Schedule();
+
+        ecbs.AddJobHandleForProducer(Dependency);
+    }
+}
